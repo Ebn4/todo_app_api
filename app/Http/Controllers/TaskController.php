77d483c8b4306de576a450e8d926c99f344788e9
Exauce Bodi <exaucebodi4@gmail.com>
2025-04-04 12:08:03 +0200

    Ajout de model, ressource task et création des api avec apiResource

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Task;
+use App\Http\Resources\TaskResource;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        $task = Task::create($request->all());
+
+        return new TaskResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::find($id);
+
+        if(!$task) {
+            return response()->json(['message' => 'Tâche non trouvé'], 404);
+        }
+        return new TaskResource($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        $task = Task::find($id);
+
+        if(!$task) {
+            return response()->json(['message' => 'Tâche non trouvé'], 404);
+        }
+
+        $task->update($request->all());
+
+        return new TaskResource($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::find($id);
+        if(!$task) {
+            return response()->json(['message' => 'Tâche non trouvé'], 404);
+        }
+        $task->delete();
+        return response()->json(['message' => 'Tâche supprimé avec succès'], 200);
+    }
+}
